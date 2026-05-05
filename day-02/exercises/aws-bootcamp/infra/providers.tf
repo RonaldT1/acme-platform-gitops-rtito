f9500de3951684a41feee40bcdda4421e82246d2
Ronald <ronaldtito1302@hotmail.com>
2026-05-05 08:34:32 -0500

    day-02: initialize structure

---
+++ day-02/exercises/aws-bootcamp/infra/providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.6"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "bootcamp-tfstate-rtito-1777929665"
+    key            = "bootcamp/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
